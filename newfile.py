24c2a271fa8a464491256ed2f3ec1502ed617ee6
fajarkurniawan-create <fajarkurniawan@students.untidar.ac.id>
2026-08-26 20:57:41 +0700

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,12 @@
+nama = "Fajar Kurniawan" #Nama lengkap
+nim = "26056007" #NIM
+ttl = "26 juni 2008,magelang" #tempat tanggal lahir
+jk = "laki laki" # jenis kelamin
+hobi = "mendengarkan musik" #hobi
+
+print ("\t\t\t\" identitas mahasiswa")
+print("nama                 : " + nama)
+print("NIM                  : " + nim)
+print("TEMPAT TANGGAL LAHIR : " + ttl)
+print("JENIS KELAMIN        : " + jk)
+print("HOBI                 : " + hobi)
