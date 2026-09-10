6c327ad44f7417fb34bafdc14956db162e6e6a99
fajarkurniawan-create <fajarkurniawan@students.untidar.ac.id>
2026-09-10 21:10:21 +0700

    Add files via upload

---
+++ 0007_fajar_operasi aritmatika.py
@@ -0,0 +1,91 @@
+# operasi aritmatika
+#a = 10
+#b = 3
+
+# operasi tambah +
+#hasil = a + b
+#print(a, '+', b, '=', hasil)
+
+# latihan konversi satuan temperature
+# program konversi celcius ke satuan lain
+
+#print("\nPROGRAM KONVERSI TEMPERATUR\n")
+
+#celcius = float(input("Masukan suhu dalam celcius : "))
+
+#print("Suhu adalah", celcius, "Celcius")
+
+# Reamur
+#reamur = (4/5) * celcius
+#print("Suhu dalam reamur adalah", reamur, "Reamur")
+
+# Fahrenheit
+#fahrenheit = ((9/5) * celcius) + 32
+#print("Suhu dalam fahrenheit adalah", fahrenheit, "Fahrenheit")
+
+# Kelvin
+#kelvin = celcius + 273
+#print("Suhu dalam kelvin adalah", kelvin, "Kelvin")
+
+# Program 3.3 Operasi Komparasi
+
+#a = 4
+#b = 2
+# Lebih besar dari >
+#print(a > b)
+
+# Kurang dari <
+#print(a < b)
+
+# Lebih besar sama dengan >=
+#print(a >= b)
+
+# Kurang dari sama dengan <=
+#print(a <= b)
+
+# Sama dengan ==
+#print(a == b)
+
+# Tidak sama dengan !=
+#print(a != b)
+
+# is
+#x = 5
+#y = 5
+#print(x is y)
+
+# is not
+#x = 5
+#y = 6
+#print(x is not y)
+
+#tugas latihan
+
+# Data bangunan
+panjang = 12
+lebar = 5
+tinggi = 8
+
+# Menghitung luas permukaan
+luas = 2 * ((panjang * lebar) +
+           (panjang * tinggi) +
+           (lebar * tinggi))
+
+# Menghitung volume
+volume = panjang * lebar * tinggi
+
+# Menghitung keliling alas
+keliling = 2 * (panjang + lebar)
+
+# Menampilkan hasil
+print("Panjang =", panjang)
+print("Lebar =", lebar)
+print("Tinggi =", tinggi)
+
+print("Luas =", luas)
+print("Volume =", volume)
+print("Keliling =", keliling)
+
+# Operasi komparasi
+print("Apakah luas lebih besar dari 50?", luas > 50)
+print("Apakah volume bernilai 480?", volume == 480)
